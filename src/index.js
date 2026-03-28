f30c25daf05ca27b87a6fec96ab70fc875df8d62
aymnr <aymnryadmhmwdmhmd@gmail.com>
2026-03-28 19:52:51 +0300

    ff

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const app = express()
 const server = http.createServer(app)
 
 const io = new Server(server, {
-  cors: { origin: 'https://chat-frontend-two-omega.vercel.app/' }
+  cors: { origin: 'https://chat-frontend-two-omega.vercel.app' }
 })
 
 app.use(cors())
